df5b41b05ca87767530b4c1636936607ee1be538
KarolinaVaz <karolinavaz@hotmail.com>
2023-10-26 23:28:48 -0300

    Validação dos formulários

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,30 +1,45 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
+import { useRef } from "react";
 import { RiSearchLine } from "react-icons/ri";
+import { useForm } from "react-hook-form";
 
-export function SearchBox(){
-    return (
-        <Flex
-        as="label"
-        flex="1"
-        py="4"
-        px="8"
-        mt="6"
-        maxWidth={400}
-        alignSelf="center"
-        color="gray.200"
-        position="relative"
-        bg="gray.800"
-        borderRadius={"full"}
-      >
-        <Input
-          color="gray.50"
-          variant="unstyled"
-          px="4"
-          mr="4"
-          placeholder="Buscar na plataforma"
-          _placeholder={{ color: "gray.400" }}
-        />
-        <Icon as={RiSearchLine} fontSize="20" />
-      </Flex>
-    )
+
+//imperativa vc declarativa
+//imperativa dizer o que deve acontecer com uma ação
+//declarativa são funções automaticas
+
+//melhores bibliotecas de form react
+//formik (mas api é muito engessada)
+//react-hook-form 
+//unform (formulários que preocupa mais com performance)
+
+export function SearchBox() {
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <Flex
+      as="label"
+      flex="1"
+      py="4"
+      px="8"
+      mt="6"
+      maxWidth={400}
+      alignSelf="center"
+      color="gray.200"
+      position="relative"
+      bg="gray.800"
+      borderRadius={"full"}
+    >
+      <Input
+        ref={searchInputRef}
+        color="gray.50"
+        variant="unstyled"
+        px="4"
+        mr="4"
+        placeholder="Buscar na plataforma"
+        _placeholder={{ color: "gray.400" }}
+      />
+      <Icon as={RiSearchLine} fontSize="20" />
+    </Flex>
+  );
 }
